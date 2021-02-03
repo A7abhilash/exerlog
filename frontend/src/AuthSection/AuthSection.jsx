ebba194bacf1625860775ae07bd3c17ac17952cf
A7abhilash <a7abhilash@gmail.com>
2021-02-03 19:19:46 +0530

    updated navbar

--- frontend/src/AuthSection/AuthSection.jsx
+++ frontend/src/AuthSection/AuthSection.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Navbar from "../containers/Navbar";
-import { useAuth } from "../contexts/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/Routes";
 
 function AuthSection() {
-  const { user } = useAuth();
-
   return (
     <Router>
       <Navbar />
--- frontend/src/containers/Navbar.jsx
+++ frontend/src/containers/Navbar.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 function Navbar() {
   const { user } = useAuth();
+  const addNew = [
+    {
+      name: "Add New Log for the Day",
+      to: "/newLog",
+    },
+    {
+      name: "Add New Exercise/Workout",
+      to: "/newExercise",
+    },
+  ];
   return (
-    <nav className="bg-light py-2 m-0">
+    <nav className="bg-light py-2 m-0 d-flex align-items-center">
       <h3 className="text-center">ExerLog</h3>
+      {user && (
+        <div className="ml-auto d-flex">
+          {addNew.map(({ name, to }) => (
+            <Link key={name} to={to} className="nav-link text-danger">
+              {name}
+            </Link>
+          ))}
+        </div>
+      )}
     </nav>
   );
 }
